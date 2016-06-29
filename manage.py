dde167f0a0460c1cbed73781d334f76fd6908b17
baby min <869947295@qq.com>
2016-06-29 14:56:18 +0800

    "git minsphoto: profile, image, comment"

---
+++ manage.py
@@ -0,0 +1,12 @@
+# -*- encoding=UTF-8 -*-
+
+from minstagram import app
+from flask_script import Manager
+
+manage = Manager(app)
+
+def init_datebase():
+
+
+if __name__ == '__main__':
+		manage.run()
---
+++ runserver.py
@@ -0,0 +1,6 @@
+# -*- encoding=UTF-8 -*-
+
+from minstagram import app
+
+if __name__ == '__main__':
+	app.run(debug=True)
